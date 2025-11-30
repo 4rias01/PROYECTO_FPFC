a5d09400f4006228a5622096695cb610e0263612
4rias01 <santiago.arias.rojas@correounivalle.edu.co>
2025-11-30 09:53:37 -0500

    itinerariosAire correctamente implementada

--- src/main/scala/Itinerarios/package.scala
+++ src/main/scala/Itinerarios/package.scala
@@ -24,23 +24,6 @@ package object Itinerarios {
     (c1: String, c2: String) => buscarItinerarios(c1, c2, Set(), List())
   }
 
-  /*
-  //Funcion auxiliar para convertir el tiempo
-  def calcularTiempoTotal(itinerario: Itinerario, aeropuertos: Map[String, Aeropuerto]): Int = {
-    itinerario.map { vuelo =>
-      val origen = aeropuertos(vuelo.Org)
-      val destino = aeropuertos(vuelo.Dst)
-      val salidaMinutos = vuelo.HS * 60 + vuelo.MS
-      val llegadaMinutos = vuelo.HL * 60 + vuelo.ML
-      val diferenciaGMT = destino.GMT - origen.GMT
-      val tiempoVuelo = (llegadaMinutos + diferenciaGMT * 60) - salidaMinutos
-      if (tiempoVuelo < 0) tiempoVuelo + 24 * 60 else tiempoVuelo
-    }.sum
-  }
-   */
-
-
-
   //3.2
   def itinerariosTiempo(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]): (String, String) => List[Itinerario] = {
 
@@ -109,43 +92,32 @@ package object Itinerarios {
 
   def itinerariosAire(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]): (String, String) => List[Itinerario] = {
 
-    def distancia(a1: Aeropuerto, a2: Aeropuerto): Double = {
-      val dx = a1.X - a2.X
-      val dy = a1.Y - a2.Y
-      Math.sqrt(dx * dx + dy * dy)
+    val aeropuertosMap = aeropuertos.map(a => a.Cod -> a).toMap
+    val itinerariosPosibles = itinerarios(vuelos, aeropuertos)
+
+    def offsetMinutos(gmt: Int): Int = (gmt / 100) * 60
+    def minutosUTC(hora: Int, minuto: Int, gmt: Int): Int = {
+      val totalMinutos = hora * 60 + minuto
+      totalMinutos - offsetMinutos(gmt)
     }
-
-    def encontrarAeropuerto(cod: String): Option[Aeropuerto] = {
-      aeropuertos.find(_.Cod == cod)
+    
+    def tiempoVuelo(vuelo: Vuelo): Double = {
+      val origen = aeropuertosMap(vuelo.Org)
+      val destino = aeropuertosMap(vuelo.Dst)
+      val salidaUTC = minutosUTC(vuelo.HS, vuelo.MS, origen.GMT)
+      val llegadaUTC = minutosUTC(vuelo.HL, vuelo.ML, destino.GMT)
+      val tiempo = llegadaUTC - salidaUTC
+      if (tiempo < 0) tiempo + 24 * 60 else tiempo
     }
-
-    def dfs(actual: String, destino: String, ruta: List[Vuelo], distTotal: Double, visitados: Set[String], mejores: collection.mutable.PriorityQueue[(List[Vuelo], Double)]): List[Itinerario] = {
-      if (actual == destino) {
-        if (mejores.size < 3) {
-          mejores.enqueue((ruta, distTotal))
-        }
-
-        else if (distTotal < mejores.head._2) {
-          mejores.dequeue()
-          mejores.enqueue((ruta, distTotal))
-        }
-        List()
-      }
-
-      else {
-        val posiblesVuelos = vuelos.filter(v => v.Org == actual && !visitados.contains(v.Dst))
-        posiblesVuelos.foldLeft(List[Itinerario]()) { (acc, vuelo) =>
-          val dist = encontrarAeropuerto(vuelo.Org).flatMap(origen => encontrarAeropuerto(vuelo.Dst).map(destino => distancia(origen, destino)))
-          dfs(vuelo.Dst, destino, ruta :+ vuelo, distTotal + dist.getOrElse(Double.MaxValue), visitados + actual, mejores) ::: acc
-        }
-      }
+    
+    def tiempoEnAire(itinerario: Itinerario): Double = {
+      itinerario.map(tiempoVuelo).sum
     }
 
     (cod1: String, cod2: String) => {
-      val mejores = collection.mutable.PriorityQueue.empty(Ordering.by[(List[Vuelo], Double), Double](_._2).reverse)
-      val visitados = Set[String]()
-      dfs(cod1, cod2, List(), 0.0, visitados, mejores)
-      mejores.map(_._1).toList
+      itinerariosPosibles(cod1, cod2).
+        sortBy(tiempoEnAire).
+        take(3)
     }
   }
 }
--- src/test/scala/Pruebas.sc
+++ src/test/scala/Pruebas.sc
@@ -68,3 +68,18 @@ val itsc4 = itsEscalasCurso("CLO", "MEX")
 
 // 2 itinerarios CTG–PTY
 val itsc5 = itsEscalasCurso("CTG", "PTY")
+
+// Obtener todos los itinerarios aéreos del curso
+val itsAireCurso = itinerariosAire(vuelosCurso, aeropuertosCurso)
+
+val itsa1 = itsAireCurso("MID", "SVCS")
+val itsa2 = itsAireCurso("CLO", "SVCS")
+
+// 4 itinerarios CLO–SVO
+val itsa3 = itsAireCurso("CLO", "SVO")
+
+// 2 itinerarios CLO–MEX
+val itsa4 = itsAireCurso("CLO", "MEX")
+
+// 2 itinerarios CTG–PTY
+val itsa5 = itsAireCurso("CTG", "PTY")
